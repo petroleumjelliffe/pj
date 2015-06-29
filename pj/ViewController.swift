35c5487b8712dea66357e7e8239c294e1198fdbd
Pete Jelliffe <pete.jelliffe@gmail.com>
2015-06-29 15:36:01 -0400

    timer start button

--- pj/ViewController.swift
+++ pj/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var button:UIButton?
+    @IBOutlet var timer:UITextField?
+    
+    var startTime = NSTimeInterval()
+    
+    var timer2 = NSTimer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,48 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func updateTime() {
+        var currentTime = NSDate.timeIntervalSinceReferenceDate()
+        var elapsedTime:NSTimeInterval = currentTime - startTime
+        
+        let minutes = UInt8(elapsedTime/60.0)
+        
+        elapsedTime -= (NSTimeInterval(minutes)*60)
+        
+        let seconds = UInt8(elapsedTime)
+        
+        elapsedTime -= NSTimeInterval(seconds)
+        
+        //find out the fraction of milliseconds to be displayed.
+        
+        let fraction = UInt8(elapsedTime * 100)
+        
+        //add the leading zero for minutes, seconds and millseconds and store them as string constants
+        
+        let strMinutes = String(format: "%02d", minutes)
+        let strSeconds = String(format: "%02d", seconds)
+        let strFraction = String(format: "%02d", fraction)
+        
+        //concatenate minuets, seconds and milliseconds as assign it to the UILabel
+        
+        self.timer?.text = "\(strMinutes):\(strSeconds):\(strFraction)"
+        
+    }
+    
+    @IBAction func startTimer(sender:AnyObject) {  //start button
+        
+        if !timer2.valid {
+            let aSelector:Selector = "updateTime"
+            timer2 = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: aSelector, userInfo: nil, repeats: true)
+            startTime = NSDate.timeIntervalSinceReferenceDate()
+        }
+        
+        
+        
+    }
+    
+    
 
 }
 
